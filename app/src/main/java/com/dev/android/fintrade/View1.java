dabedc147cc1bd68118015a61702af018471aee0
DevP-ai <patar1447@gmail.com>
2023-02-25 23:47:24 +0530

    SignUp Form Updated

--- app/src/main/java/com/dev/android/fintrade/View1.java
+++ app/src/main/java/com/dev/android/fintrade/View1.java
@@ -2,13 +2,29 @@ package com.dev.android.fintrade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class View1 extends AppCompatActivity {
+
+    LinearLayout signup_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view1);
+
+        signup_btn=findViewById(R.id.SignUp_btn);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(View1.this,SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
